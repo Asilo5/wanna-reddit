606b411ab1d02603157c0024a4c0b162f325cc84
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-04 18:40:59 -0700

    First commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const orm = console.log('hello wkforld');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { MikroORM } from '@mikro-orm/core'; 
+
+const orm = 
+
+console.log('hello wkforld')
